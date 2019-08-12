bf718bc8a96a3f2152468ae47a4f73b8a2eb294f
Guilherme <guiaszul@gmail.com>
2019-08-12 00:05:44 -0300

    FirstButton
    Make button without StoryBoard and add Contrains

--- TestViewCode/TestViewCode/ViewControllerScreen.swift
+++ TestViewCode/TestViewCode/ViewControllerScreen.swift
@@ -12,7 +12,24 @@ final class ViewControllerScreen: UIView{
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        //Make Button
+        let button = UIButton(frame: .zero)
+        button.backgroundColor = #colorLiteral(red: 0.2437559962, green: 0, blue: 0.01320731547, alpha: 1)
+        button.setTitle("Smash3", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Add SubView
+        addSubview(button)
+        //Set Contrains
+        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        //Extra Configs
+        
+        
+        backgroundColor = .darkGray
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
