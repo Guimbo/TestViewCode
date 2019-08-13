d5b493e0468605a2a2321975cfe0af8ee9853883
Guilherme <guiaszul@gmail.com>
2019-08-13 15:03:31 -0300

    Creating Protocol and Extension Protocol

---
+++ TestViewCode/TestViewCode/CodeView.swift
@@ -0,0 +1,31 @@
+//
+//  CodeView.swift
+//  TestViewCode
+//
+//  Created by Guilherme Araujo on 13/08/19.
+//  Copyright © 2019 Guilherme Araujo. All rights reserved.
+//
+
+import UIKit
+
+protocol CodeView: UIView{
+    func buildViewsInHierarchy()
+    func setContrains()
+    func setExtraConfigs()
+    func setupView()
+    func makeAnything()
+}
+
+extension CodeView{
+    func setupView(){
+        buildViewsInHierarchy()
+        setContrains()
+        setExtraConfigs()
+    }
+    
+    
+    func makeAnything() {
+        print("Make Anything")
+    }
+    
+}
--- TestViewCode/TestViewCode/ViewControllerScreen.swift
+++ TestViewCode/TestViewCode/ViewControllerScreen.swift
@@ -9,30 +9,41 @@
 import UIKit
 
 final class ViewControllerScreen: UIView{
+    
+    lazy var button:UIButton = {
+        let view = UIButton(frame: .zero)
+        view.backgroundColor = #colorLiteral(red: 0.2437559962, green: 0, blue: 0.01320731547, alpha: 1)
+        view.setTitle("Smash", for: .normal)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
-        
-        //Make Button
-        let button = UIButton(frame: .zero)
-        button.backgroundColor = #colorLiteral(red: 0.2437559962, green: 0, blue: 0.01320731547, alpha: 1)
-        button.setTitle("Smash3", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        //Add SubView
-        addSubview(button)
-        //Set Contrains
-        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        //Extra Configs
-        
-        
-        backgroundColor = .darkGray
-        
+        setupView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension ViewControllerScreen: CodeView{
+    func buildViewsInHierarchy() {
+        addSubview(button)
+    }
+    
+    func setContrains() {
+        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func setExtraConfigs() {
+        backgroundColor = .darkGray
+    }
+    
+    
 }
